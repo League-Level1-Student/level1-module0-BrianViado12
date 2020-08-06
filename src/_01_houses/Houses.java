5071e348f1699b2e43d929a4d89fc1ee38314494
Daddy <Daddy@vivs-macpro-13>
2020-08-06 15:56:00 -0700

    did this 8/6

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -16,9 +16,13 @@ public class Houses {
 		builder.setSpeed(50);
 		builder.setPenWidth(10);
 		
-		for (int i = 0; i < 9; i++) {
+		for (int i = 0; i < 7; i++) {
 			build(100);
+			
 		}
+		build("small", "green");
+		build("medium", "red");
+		build("large", "blue");
 	}
 	 void build(int height) {
 		builder.move(height);
@@ -32,5 +36,35 @@ public class Houses {
 		builder.setPenColor(Color.black);
 		builder.turn(270);
 	}
+	 void build(String size, String color) {
+			int height = 0;
+		 if(size.equals("small")) {
+			 height = 60;
+		 } else if (size.equals("medium")) {
+			 height = 120;
+		 } else if (size.equals("large")) {
+			 height = 250;
+		 }
+		 if (color.equals("red")) {
+			 builder.setPenColor(Color.red);
+		 } else if (color.equals("green")) {
+			 builder.setPenColor(Color.green);
+		 } else if (color.equals("blue")) {
+			 builder.setPenColor(Color.blue);
+		 } else if (color.equals("yellow")) {
+			 builder.setPenColor(Color.yellow);
+		 }
+			builder.move(height);
+			builder.turn(90);
+			builder.move(40);
+			builder.turn(90);
+			builder.move(height);
+			builder.setPenColor(Color.green);
+			builder.turn(-90);
+			builder.move(35);
+			builder.setPenColor(Color.black);
+			builder.turn(270);
+	 }
+	 
 }
  
