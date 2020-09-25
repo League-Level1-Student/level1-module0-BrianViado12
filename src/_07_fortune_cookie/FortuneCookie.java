bce7d35c32f25cf5a3d12cf0ffe09526b50126ea
Daddy <Daddy@vivs-macpro-13>
2020-09-24 17:17:16 -0700

    9/24

--- src/_07_fortune_cookie/FortuneCookie.java
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -1,8 +1,44 @@
 package _07_fortune_cookie;
 
-public class FortuneCookie {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class FortuneCookie implements ActionListener {
         public void showButton() {
-            System.out.println("Button clicked");
+        	System.out.println("Button clicked");
+        	JFrame frame = new JFrame();
+            frame.setVisible(true);
+            JButton button = new JButton();
+            frame.add(button);
+            frame.pack();
+            button.addActionListener(this);
+            int rand = new Random().nextInt(5);	
+            if(rand==(0)) {
+            	
+            }
+            if(rand==(1)) {
+            	
+            }
+            if(rand==(2)) {
+            	
+            }
+            if(rand==(3)) {
+            	
+            }
+            if(rand==(4)) {
+            	
+            }
        }
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			JOptionPane.showMessageDialog(null, "Woohoo");
+		}
     }
 
